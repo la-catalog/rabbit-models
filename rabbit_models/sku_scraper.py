ebde3768daabe8f6bd751d2551063eaf8008c9ac
thiagola92 <thiagola92@gmail.com>
2022-05-28 03:53:14 -0300

    Add source property to Metadata from sku_scraper

--- rabbit_models/sku_scraper.py
+++ rabbit_models/sku_scraper.py
@@ -8,11 +8,13 @@ class Metadata(BaseModel):
     created - When the message was created
     query - The query which the SKU came from
     attempts - How many times the message have been requeued
+    source - Who started this pipeline
     """
 
     created: datetime = datetime.utcnow()
     query: str | None = None
     attempts: int | None = None
+    source: str | None = None
 
 
 class Body(BaseModel):
