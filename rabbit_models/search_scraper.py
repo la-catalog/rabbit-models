a109ac90e443c297511570f385166612414f6e2b
thiagola92 <thiagola92@gmail.com>
2022-05-28 04:15:55 -0300

    Add search_scraper

---
+++ rabbit_models/search_scraper.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from pydantic import AnyHttpUrl, BaseModel
+
+
+class Metadata(BaseModel):
+    """
+    created - When the message was created
+    query - The query which the SKU came from
+    attempts - How many times the message have been requeued
+    source - Who started this pipeline
+    """
+
+    created: datetime = datetime.utcnow()
+    query: str | None = None
+    attempts: int | None = None
+    source: str | None = None
+
+
+class Body(BaseModel):
+    """
+    urls - List of urls needed to be visited to obtain a SKU
+    marketplace - Marketplace configuration to use
+    metadata - Data about this message
+    """
+
+    url: AnyHttpUrl
+    marketplace: str
+    metadata: Metadata = Metadata()
